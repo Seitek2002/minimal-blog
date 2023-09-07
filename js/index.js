84e31d33b0aa8f5acf1b4487ceb0a468d45a04f1
Seitek2002 <seitek_2002@bk.ru>
2023-09-07 19:14:20 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+document.querySelector(".burger-lines").addEventListener("click", () => {
+    document.querySelector(".burger-menu").classList.toggle("active");
+    document.querySelector("body").classList.toggle("active");
+});
+
